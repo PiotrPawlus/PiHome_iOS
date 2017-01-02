dc283466043eeeeaff9adf99a9c8ec4a49a73bf3
Piotr Pawluś <pawlus.piotrek@gmail.com>
2017-01-02 21:33:32 +0100

    Ui tests for connect, login and register.

---
+++ PiHomeUITests/Extensions/XCUIElement+Extension.swift
@@ -0,0 +1,63 @@
+//
+//  XCUIElement+Extension.swift
+//  PiHome
+//
+//  Created by Piotr Pawluś on 02/01/2017.
+//  Copyright © 2017 Piotr Pawluś. All rights reserved.
+//
+
+import XCTest
+
+extension XCUIElement {
+    
+    //MARK: - Class Methods
+    
+    //MARK: - Initialization
+    
+    //MARK: - Deinitialization
+    
+    //MARK: - Actions
+    
+    //MARK: - Open
+    
+    //MARK: - Internal
+    
+    func confirm() {
+        buttons["OK"].tap()
+    }
+    
+    func clear() {
+        
+        coordinate(withNormalizedOffset: CGVector(dx: 0.9, dy: 0.9)).tap()
+        
+        let deleteString = [String](repeating: XCUIKeyboardKeyDelete, count: (value as! String).characters.count + 1)
+        typeText(deleteString.joined(separator: ""))
+    }
+    
+    func pressReturn() {
+        
+        tap()
+        typeText("\n")
+    }
+    
+    func tapCell(atIndex index: UInt) {
+        cells.element(boundBy: index).tap()
+    }
+    
+    func type(text: String) {
+        
+        tap()
+        typeText(text)
+    }
+    
+    func tapIfExists() {
+        
+        if exists {
+            tap()
+        }
+    }
+    
+    //MARK: - Private
+    
+    //MARK: - Overridde
+}
--- PiHomeUITests/PiHomeUITests.swift
+++ PiHomeUITests/PiHomeUITests.swift
@@ -9,28 +9,201 @@
 import XCTest
 
 class PiHomeUITests: XCTestCase {
-        
+    
+    private let app = XCUIApplication()
+    
+    //Alerts
+    
+    private var confirmationPasswordAlert: XCUIElement!
+    private var connectAletr: XCUIElement!
+    private var emailAlert: XCUIElement!
+    private var firstNameAlert: XCUIElement!
+    private var lastNameAlert: XCUIElement!
+    private var passwordAlert: XCUIElement!
+    private var passwordsAlert: XCUIElement!
+
+    //BarButtonItems
+    
+    private var backBarButtonItem: XCUIElement!
+    
+    //Buttons
+    
+    private var connectButton: XCUIElement!
+    private var signInButton: XCUIElement!
+    private var signUpButton: XCUIElement!
+    private var submitButton: XCUIElement!
+    
+    //TextFields
+    
+    private var addressTextField: XCUIElement!
+    private var confirmPasswordTextField: XCUIElement!
+    private var emailTextField: XCUIElement!
+    private var firstNameTextField: XCUIElement!
+    private var lastNameTextField: XCUIElement!
+    private var passwordTextField: XCUIElement!
+    
+    //StaticText
+    
+    private var informationStaticText: XCUIElement!
+    
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        
+        app.launchEnvironment = ["animations": "0"]
+        app.launch()
+        
+        //Alerts
+        
+        confirmationPasswordAlert = app.alerts["Confirm password"]
+        connectAletr = app.alerts["Server Address"]
+        emailAlert = app.alerts["Email"]
+        firstNameAlert = app.alerts["First Name"]
+        lastNameAlert = app.alerts["Last Name"]
+        passwordAlert = app.alerts["Password"]
+        passwordsAlert = app.alerts["Passwords"]
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        //BarButtonItems
+        
+        backBarButtonItem = app.buttons["backBarButtonItem"]
+        
+        //Buttons
+        
+        connectButton = app.buttons["connectButton"]
+        signInButton = app.buttons["signInButton"]
+        signUpButton = app.buttons["signUpButton"]
+        submitButton = app.buttons["submitButton"]
+        
+        //TextFields
+        
+        addressTextField = app.textFields["addressTextField"]
+        confirmPasswordTextField = app.secureTextFields["confirmPasswordTextField"]
+        emailTextField = app.textFields["emailTextField"]
+        firstNameTextField = app.textFields["firstNameTextField"]
+        lastNameTextField = app.textFields["lastNameTextField"]
+        passwordTextField = app.secureTextFields["passwordTextField"]
+        
+        //StaticText
+        
+        informationStaticText = app.staticTexts["informationStaticText"]
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    //MARK: - Class Methods
+    
+    //MARK: - Initailization
+    
+    //MARK: - Deinitialization
+    
+    //MARK: - Actions
+    
+    //MARK: - Public
+    
+    //MARK: - Internal
+    
+    func testPiHome() {
+        
+        backBarButtonItem.tap()
+        
+        connectButton.tap()
+        
+        XCTAssertTrue(connectAletr.exists)
+        
+        connectAletr.confirm()
+        addressTextField.pressReturn()
+        
+        XCTAssertTrue(connectAletr.exists)
+        
+        connectAletr.confirm()
+        addressTextField.tap()
+        informationStaticText.tap()
+        addressTextField.type(text: "www.example.com")
+        connectButton.tap()
+        
+        backBarButtonItem.tapIfExists()
+        addressTextField.type(text: "www.example.com")
+        connectButton.tap()
+        
+        emailTextField.pressReturn()
+        passwordTextField.pressReturn()
+        
+        XCTAssertTrue(emailAlert.exists)
+        
+        emailAlert.confirm()
+        emailTextField.type(text: "john.little")
+        informationStaticText.tap()
+        signInButton.tap()
+        
+        XCTAssertTrue(emailAlert.exists)
+        
+        emailAlert.confirm()
+        emailTextField.clear()
+        emailTextField.type(text: "john.little@example.com")
+        signInButton.tap()
+        
+        XCTAssertTrue(passwordAlert.exists)
+        
+        passwordAlert.confirm()
+        passwordTextField.tap() // TO DO - type password and logout
+        
+        signUpButton.tap()
+        backBarButtonItem.tap()
+        signUpButton.tap()
+        
+        firstNameTextField.pressReturn()
+        lastNameTextField.pressReturn()
+        emailTextField.pressReturn()
+        passwordTextField.pressReturn()
+        confirmPasswordTextField.pressReturn()
+        
+        XCTAssertTrue(firstNameAlert.exists)
+        
+        firstNameAlert.confirm()
+        firstNameAlert.tap()
+        informationStaticText.tap()
+        
+        firstNameTextField.type(text: "John")
+        submitButton.tap()
+        
+        XCTAssertTrue(lastNameAlert.exists)
+        
+        lastNameAlert.confirm()
+        lastNameTextField.type(text: "Little")
+        submitButton.tap()
+        
+        XCTAssertTrue(emailAlert.exists)
+        
+        emailAlert.confirm()
+        emailTextField.type(text: "john.little")
+        submitButton.tap()
+        
+        XCTAssertTrue(emailAlert.exists)
+        
+        emailAlert.confirm()
+        emailTextField.clear()
+        emailTextField.type(text: "john.little@example.com")
+        
+        submitButton.tap()
+        
+        XCTAssertTrue(passwordAlert.exists)
+        
+        passwordAlert.confirm()
+        passwordTextField.type(text: "Password1")
+        submitButton.tap()
+        
+        XCTAssertTrue(confirmationPasswordAlert.exists)
+        
+        confirmationPasswordAlert.confirm()
+        confirmPasswordTextField.type(text: "password")
+        submitButton.tap()
+        
+        XCTAssertTrue(passwordsAlert.exists)
+        
+        passwordsAlert.confirm()
+        confirmPasswordTextField.type(text: "Password1")
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    //MARK: - Private
     
+    //MARK: - Overridden
 }
